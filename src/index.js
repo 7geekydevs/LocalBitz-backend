f5c9a8e675c6e0d4268556018fb6780574098f71
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-08-29 17:29:19 +0530

    connectionwith mongodb , menu item model , post menu item req

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+//packages
 const express = require('express')
 
+//router files
 const menuRouter = require('./routes/menu_item_routes')
 
+//db connection
+require('./db/db_connection')
+
 const app = express()
+
+app.use(express.json())
 
 app.use(menuRouter)
 
--- src/routes/menu_item_routes.js
+++ src/routes/menu_item_routes.js
@@ -1,17 +1,31 @@
+//packages
 const express = require('express')
 
+//router
 const router = express.Router()
+
+//models
+const menuItemModel = require('../models/menu_item_model')
 
 router.get('/menu' , (req, res) =>{
     res.send('get menu endpoint')
 })
 
-router.post('/menu' , (req, res) =>{
-    res.send('post menu endpoint')
+router.post('/menu' , async (req, res) =>{
+
+    const menuItem = new menuItemModel(req.body)
+
+    try{
+        await menuItem.save()
+        res.status(201).send(menuItem)
+    }catch(e){
+        res.status(400).send(e)
+    }
+
 })
 
 router.patch('/menu' , (req, res) =>{
-    res.send('patch menu endpoint')
+    res.send(req.body)
 })
 
 router.delete('/menu' , (req, res) =>{

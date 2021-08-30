88be371757e9c66e4f6143846ee42494b4a3ee2c
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-08-30 13:55:30 +0530

    env variables bug fix : bypassing validation in patch

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-    'mongodb://127.0.0.1:27017/localbitz',
+    process.env.MONGODB_URL,
     {
         useNewUrlParser : true,
         useUnifiedTopology: true
--- src/routes/menu_item_routes.js
+++ src/routes/menu_item_routes.js
@@ -27,12 +27,29 @@ router.post('/menu' , async (req, res) =>{
 
 
 router.patch('/menu/:id' , async (req, res) =>{
+
+    const allowedUpdates = ['name' , 'price' , 'rating']
+    const updates = Object.keys(req.body)
+
+    const isOperationValid = updates.every((update) => allowedUpdates.includes(update))
+
+    // await menuItemModel.findByIdAndUpdate(req.params.id , req.body)
+    // const menuItem = await menuItemModel.findById(req.params.id)
+
+    if(!isOperationValid){
+        res.status(400).send({'Error' : 'Invalid Updates'})
+    }
+
     try{
-    await menuItemModel.findByIdAndUpdate(req.params.id , req.body)
-    const menuItem = await menuItemModel.findById(req.params.id)
-    res.send(menuItem)
+        const item = await menuItemModel.findById(req.params.id)
+        updates.forEach((update) => {
+            item[update] = req.body[update]
+        }
+        )
+        await item.save()
+        return res.send(item)
     }catch(e){
-        res.status(400).send(e)
+        res.status(500).send(e)
     }
 
 })

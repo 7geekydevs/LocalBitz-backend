d52702eac01c1591958518f2dafc6c58abcdaeb7
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-11-12 19:33:34 +0530

    cors support added

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
 const express = require('express')
-
+const cors = require('cors')
 
 const menuRouter = require('./src/routes/menu_item_routes')
 const cookRouter = require('./src/routes/cook_routes')
 const customerRouter = require('./src/routes/customer_routes')
 const orderRouter = require('./src/routes/order_routes')
 
-
 require('./src/db/db_connection')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(menuRouter)
 app.use(cookRouter)
@@ -22,8 +21,3 @@ app.use(orderRouter)
 //This is for production
 module.exports = app
 
-//This is for development
-// const port = process.env.PORT
-// app.listen(port , () =>{
-//     console.log('server up on ' + port)
-// })
